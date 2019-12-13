d3d34d584ce64aa6dc3c41a84ddf63403594f1c9
vagrant <vagrant@homestead>
2019-12-13 07:03:50 +0000

    extend Logger

--- src/Test.php
+++ src/Test.php
@@ -1,9 +1,11 @@
 <?php
 
 
-class First_package
+class First_package extends \Monolog\Logger
 {
+
     public function hey(){
         echo '<h1>HEY!</h1>';
+        \Monolog\Logger::ALERT;
     }
 }
